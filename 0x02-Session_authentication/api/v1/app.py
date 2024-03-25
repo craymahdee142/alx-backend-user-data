ab043b8c5e8036f584d0c3cfde04f6d4c85fd426
root <root@DESKTOP-H9275MT.localdomain>
2024-03-25 17:16:04 +0000

    Session authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -13,23 +13,31 @@ from flask_cors import CORS, cross_origin
 
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
+from api.v1.auth.session_db_auth import SessionDBAuth
+from api.v1.auth.session_exp_auth import SessionExpAuth
 from api.v1.views import app_views
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-# Create a variable auth initialized to None after the CORS definition
+# Create a auth initialized to None after the CORS definition
 auth = None
 
 
-# Update api/v1/app.py for using BasicAuth class instead of Auth depending
-# on the value of the environment variable AUTH_TYPE, If AUTH_TYPE is equal
-# to basic_auth:
-#   import BasicAuth from api.v1.auth.basic_auth
-#   create an instance of BasicAuth and assign it to the variable auth
-# Otherwise, keep the previous mechanism with auth an instance of Auth.
+# Update api/v1/app.py for using SessionAuth instance 
+# value of the environment variable AUTH_TYPE, If
+# AUTH_TYPE is equal to session_auth:
+#   import SessionAuth from api.v1.auth.session_auth
+#   instance of SessionAuth and assign it to the variable auth
 auth_type = getenv('AUTH_TYPE', 'default')
-if auth_type == "basic_auth":
+if auth_type == "session_auth":
+    auth = SessionAuth()
+elif auth_type == 'session_exp_auth':
+    auth = SessionExpAuth()
+elif auth_type == 'session_db_auth':
+    auth = SessionDBAuth()
+elif auth_type == "basic_auth":
     auth = BasicAuth()
 else:
     auth = Auth()
@@ -50,7 +58,7 @@ def unauthorized(error: Exception) -> Tuple[jsonify, int]:
         error (Exception): The error raised.
 
     Returns:
-        Tuple[jsonify, int]: JSON response with the error message and a 401
+        Tuple[jsonify, int]: JSON response with the error 401
         status code.
     """
     return jsonify({"error": "Unauthorized"}), 401
@@ -64,7 +72,7 @@ def forbidden(error: Exception) -> Tuple[jsonify, int]:
         error (Exception): The error raised.
 
     Returns:
-        Tuple[jsonify, int]: JSON response with the error message and a 401
+        Tuple[jsonify, int]: JSON response wit error 401
         status code.
     """
     return jsonify({"error": "Forbidden"}), 403
@@ -73,7 +81,7 @@ def forbidden(error: Exception) -> Tuple[jsonify, int]:
 @app.before_request
 def handle_request():
     """
-    Handle the request by checking for authentication and authorization.
+    Handle req for checking authentication and authorization.
     """
     # If auth is None, do nothing
     if auth is None:
@@ -81,21 +89,25 @@ def handle_request():
     # Create list of excluded paths
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
     # if request.path is not part of the list above, do nothing
     # You must use the method require_auth from the auth instance
     if not auth.require_auth(request.path, excluded_paths):
         return
-    # If auth.authorization_header(request) returns None, raise the error
-    # 401 - you must use abort
+    # If auth.authorization_header(request)
+    # return None, raise the error, 401 - you must use abort
     auth_header = auth.authorization_header(request)
-    if auth_header is None:
+    session_cookie = auth.session_cookie(request)
+    if auth_header is None and session_cookie is None:
         abort(401)
-    # If auth.current_user(request) returns None, raise the error 403 - you
+    # If auth.current_user(request) returns None
     # must use abort
     user = auth.current_user(request)
     if user is None:
         abort(403)
+    # result of auth.current_user(request) to request.current_user
+    request.current_user = user
 
 
 if __name__ == "__main__":
